697aa9091c16f1b9ea93fd090c8c140f17fbc7ea
Swapnil Bawkar <swapnil.bawkar@ingramcontent.com>
2016-09-27 23:01:19 +0530

    added video recording API

--- EReader/src/components/MediaRecord.js
+++ EReader/src/components/MediaRecord.js
@@ -5,42 +5,98 @@ class MediaRecord extends Component {
     constructor(props, context) {
         super(props, context);
         this.initializeMediaRecorder();
-
-        this.start = this.start.bind(this);
+        this.state = {
+            reccordedBlob: [],
+            sourceBuffer: []
+        };
+        this.toggleRecording = this.toggleRecording.bind(this);
+        this.play = this.play.bind(this);
         this.stop = this.stop.bind(this);
-
     }
 
     initializeMediaRecorder() {
+        this.reccordedBlob = [];
         const constraints = {
             audio: true,
             video: true
         };
         navigator.mediaDevices.getUserMedia(constraints)
-            .then(this.handleSuccess).catch(this.handleError);
+            .then((stream) => this.handleSuccess(stream)).catch(this.handleError);
     }
 
     handleSuccess(stream) {
         console.log('getUserMedia() got stream: ', stream);
         window.stream = stream;
-        const options = {mimeTypes: 'video/webm;codecs=vp9'};
-        this.mediaRecorder = new window.MediaRecorder(window.stream, options);
-
-        let recordedChuncks = [];
-        this.mediaRecorder.ondataavailable = function (event) {
-            if(event.data.size > 0) {
-                recordedChuncks.push(event.data);
-            }
+        if (window.URL) {
+            this.gumVideo.src = window.URL.createObjectURL(stream);
+        } else {
+            this.gumVideo.src = stream;
         }
     }
 
+    handleDataAvailable(event) {
+        if(event.data.size > 0) {
+            this.recordedBlobs.push(event.data);
+        }
+    }
 
     handleError(error) {
         console.log('navigator.getUserMedia error: ', error);
     }
 
-    start() {
-        this.mediaRecorder.start();
+    toggleRecording() {
+        if (this.recordButton.textContent === 'Start Recording') {
+            this.startRecording();
+        } else {
+            this.stopRecording();
+            this.recordButton.textContent = 'Start Recording';
+            this.playButton.disabled = false;
+        }
+    }
+
+    stopRecording() {
+        this.mediaRecorder.stop();
+        console.log('Recorded Blobs: ', this.recordedBlobs);
+        this.recordedVideo.controls = true;
+    }
+
+    startRecording() {
+        this.recordedBlobs = [];
+        var options = {mimeType: 'video/webm;codecs=vp9'};
+        if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+            console.log(options.mimeType + ' is not Supported');
+            options = {mimeType: 'video/webm;codecs=vp8'};
+            if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+                console.log(options.mimeType + ' is not Supported');
+                options = {mimeType: 'video/webm'};
+                if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+                    console.log(options.mimeType + ' is not Supported');
+                    options = {mimeType: ''};
+                }
+            }
+        }
+        try {
+            this.mediaRecorder = new MediaRecorder(window.stream, options);
+        } catch (e) {
+            console.error('Exception while creating MediaRecorder: ' + e);
+            alert('Exception while creating MediaRecorder: '
+                + e + '. mimeType: ' + options.mimeType);
+            return;
+        }
+        console.log('Created MediaRecorder', this.mediaRecorder, 'with options', options);
+        this.recordButton.textContent = 'Stop Recording';
+        this.playButton.disabled = true;
+        this.mediaRecorder.onstop = this.handleStop;
+        this.mediaRecorder.ondataavailable = (event) => {
+            this.handleDataAvailable(event);
+        };
+        this.mediaRecorder.start(10); // collect 10ms of data
+        console.log('MediaRecorder started', this.mediaRecorder);
+    }
+
+    play() {
+        var superBuffer = new Blob(this.recordedBlobs, {type: 'video/webm'});
+        this.recordedVideo.src = window.URL.createObjectURL(superBuffer);
     }
 
     stop() {
@@ -50,8 +106,12 @@ class MediaRecord extends Component {
     render() {
         return (
             <div>
-                <div onClick={this.start}>Start</div>
-                <div onClick={this.stop}>Stop</div>
+                <video id="gum" ref={(c)=> this.gumVideo = c} autoPlay muted></video>
+                <video id="recorded" ref={(c)=> this.recordedVideo = c} autoPlay loop></video>
+                <div>
+                    <button id="record" ref={(c)=> this.recordButton = c} onClick={this.toggleRecording}>Start Recording</button>
+                    <button id="play" ref={(c)=> this.playButton = c} onClick={this.play}>Play</button>
+                </div>
             </div>
         );
     }
