7bc8d0d591549bd63cca2d52fd31f43139e154b2
Swapnil Bawkar <swapnil.bawkar@ingramcontent.com>
2016-09-28 11:00:47 +0530

    Added ebook

--- EReader/src/App.js
+++ EReader/src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
-import MediaRecord from './components/MediaRecord';
 import './App.css';
 
+/* globals Jigsaw */
+
+let config = {
+  vbid: "LF9780170130653-03",
+  el: "book",
+  page: "recent",
+  brand: "vitalsource",
+  zoomDisabled: false,
+  swipeDisabled: false,
+  highlightsEnabled: false
+};
+
 class App extends Component {
+  componentDidMount() {
+    this.viewer = Jigsaw.createPageViewer(config,  (data) => {
+      if(data.code === 200) {
+        console.log(data);
+        this.viewer.bind('book:selectionWithPosition', (event) => {
+          console.log(event);
+          this.viewer.Book.highlightCurrentSelection({markerId: 'sample'}, (highlighter) => {
+            console.log(highlighter);
+          });
+        });
+      }
+    });
+  }
+
   render() {
     return (
-      <MediaRecord/>
+      <div id="book"></div>
     );
   }
 }
