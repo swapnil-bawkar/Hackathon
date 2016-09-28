da3239d2115d068f0c3c22c6fd7cb87de89a5caf
Swapnil Bawkar <swapnil.bawkar@ingramcontent.com>
2016-09-28 11:57:55 +0530

    Added modal dialog

--- EReader/src/App.js
+++ EReader/src/App.js
@@ -1,38 +1,55 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import {ModalContainer, ModalDialog} from 'react-modal-dialog';
+import MediaRecorder from './components/MediaRecord';
 
 /* globals Jigsaw */
 
 let config = {
-  vbid: "LF9780170130653-03",
-  el: "book",
-  page: "recent",
-  brand: "vitalsource",
-  zoomDisabled: false,
-  swipeDisabled: false,
-  highlightsEnabled: false
+    vbid: "9781285840802",
+    el: "book",
+    page: "recent",
+    brand: "vitalsource",
+    zoomDisabled: false,
+    swipeDisabled: false,
+    highlightsEnabled: false
 };
 
 class App extends Component {
-  componentDidMount() {
-    this.viewer = Jigsaw.createPageViewer(config,  (data) => {
-      if(data.code === 200) {
-        console.log(data);
-        this.viewer.bind('book:selectionWithPosition', (event) => {
-          console.log(event);
-          this.viewer.Book.highlightCurrentSelection({markerId: 'sample'}, (highlighter) => {
-            console.log(highlighter);
-          });
-        });
-      }
-    });
-  }
+    state = {
+        isShowingModal: false
+    }
 
-  render() {
-    return (
-      <div id="book"></div>
-    );
-  }
+    componentDidMount() {
+        this.viewer = Jigsaw.createPageViewer(config, (data) => {
+            if (data.code === 200) {
+                console.log(data);
+                this.viewer.bind('book:selectionWithPosition', (event) => {
+                    this.setState({isShowingModal: true});
+                    this.viewer.Book.highlightCurrentSelection({markerId: 'sample'}, (highlighter) => {
+                        console.log(highlighter);
+                    });
+                });
+            }
+        });
+    }
+
+    handleClose = () => this.setState({isShowingModal: false})
+
+    render() {
+        return <div id="book-container" >
+                <div id="book"></div>
+            {
+                this.state.isShowingModal &&
+                <ModalContainer onClose={this.handleClose}>
+                    <ModalDialog onClose={this.handleClose}>
+                        <h1>Dialog Content</h1>
+                        <MediaRecorder></MediaRecorder>
+                    </ModalDialog>
+                </ModalContainer>
+            }
+        </div>;
+    }
 }
 
 export default App;
